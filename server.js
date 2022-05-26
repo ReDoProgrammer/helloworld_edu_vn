4b38aeb98a247c04be11c4c8a8274b9021121c35
ReDo Programmer <redoprogrammer@gmail.com>
2022-05-26 11:17:25 +0700

    change port

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ app.set("view engine","ejs");
 app.set("views","./views");
 const layout = require("express-ejs-layouts");
 app.use(layout);
-app.listen(9000,_=>{
-    console.log(`Server is running on PORT: ${9000}`);
+app.listen(9099,_=>{
+    console.log(`Server is running on PORT: ${9099}`);
 });
 
 
