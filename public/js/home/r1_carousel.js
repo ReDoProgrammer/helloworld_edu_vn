88edde52b1ad35703bac63cda7cb5e5e99901d48
ReDo Programmer <redoprogrammer@gmail.com>
2022-05-27 10:10:35 +0700

    render jsx with babel from array

--- public/js/home/r1_carousel.js
+++ public/js/home/r1_carousel.js
@@ -1,131 +1,119 @@
+const postsList = [
+    {
+        thumb: {
+            src: 'assets/img/post/1.png',
+            header: 'Tech'
+        },
+        detail: {
+            title: 'The FAA will test drone detecting technologies in airports this year',
+            timespan: '08.22.2020'
+        }
+    },
+    {
+        thumb: {
+            src: 'assets/img/post/2.png',
+            header: 'Food'
+        },
+        detail: {
+            title: 'Rocket Lab will resume launches no sooner than August 27th',
+            timespan: '08.22.2020'
+        }
+    },
+    {
+        thumb: {
+            src: 'assets/img/post/3.png',
+            header: 'Food'
+        },
+        detail: {
+            title: 'Google Drive flaw may attackers fool you into install malware',
+            timespan: '08.22.2020'
+        }
+    },
+    {
+        thumb: {
+            src: 'assets/img/post/4.png',
+            header: 'Tech'
+        },
+        detail: {
+            title: 'TikTok will sue the US over threatened ban',
+            timespan: '08.22.2020'
+        }
+    }
+];
+
 const carousel = React.createElement(
     'div', {
     className: 'row'
 },
-    React.createElement(
-        'div', {
-        className: 'col-lg-3 col-sm-6'
-    },
-        React.createElement(
-            'div', {
-            className: 'single-post-wrap style-white'
-        },
+    postsList.map((p, index) => {
+        return (
             React.createElement(
                 'div', {
-                className: 'thumb'
+                className: 'col-lg-3 col-sm-6',
+                key: index
             },
-                React.createElement(
-                    'img', {
-                    src: 'assets/img/post/1.png',
-                    alt: 'img'
-                }
-                )
-            ),
-            React.createElement(
-                'div', {
-                className: 'details'
-            },
-                React.createElement(
-                    'h6', {
-                    className: 'title'
-                },
-                    React.createElement(
-                        'a', {
-                        href: '#',
-
-                    },
-                        'The FAA will test drone detecting technologies in airports this year'
-                    )
-                ),
                 React.createElement(
                     'div', {
-                    className: 'post-meta-single mt-3'
+                    className: 'single-post-wrap style-white'
                 },
                     React.createElement(
-                        'ul', {},
+                        'div', {
+                        className: 'thumb'
+                    },
                         React.createElement(
-                            'li', {},
-                            React.createElement(
-                                'i', {
-                                className: 'fa fa-clock-o'
+                            'img', {
+                            src: p.thumb.src,
+                            alt: 'img'
+                        }
+                        ),
+                        React.createElement(
+                            'a',{
+                                className:'tag-base tag-blue'
                             },
-                                '08.22.2020'
+                            p.thumb.header
+                        )
+                    ),
+                    React.createElement(
+                        'div', {
+                        className: 'details'
+                    },
+                        React.createElement(
+                            'h6', {
+                            className: 'title'
+                        },
+                            React.createElement(
+                                'a', {
+                                href: '#',
+
+                            },
+                                p.detail.title
+                            )
+                        ),
+                        React.createElement(
+                            'div', {
+                            className: 'post-meta-single mt-3'
+                        },
+                            React.createElement(
+                                'ul', {},
+                                React.createElement(
+                                    'li', {},
+                                    React.createElement(
+                                        'i', {
+                                        className: 'fa fa-clock-o'
+                                    },
+                                        p.detail.timespan
+                                    )
+                                )
                             )
                         )
                     )
                 )
+
             )
         )
-
-    )
+    })
 )
 
 const r1_carousel = document.getElementById('r1_carousel');
 ReactDOM.render(carousel, r1_carousel)
 
-{/* <div className="row" >
-        <div className="col-lg-3 col-sm-6">
-            <div className="single-post-wrap style-white">
-                <div className="thumb">
-                    <img src="assets/img/post/1.png" alt="img"/>
-                        <a className="tag-base tag-blue" href="#">Tech</a>
-                </div>
-                <div className="details">
-                    <h6 className="title"><a href="#">The FAA will test drone detecting technologies in airports this year</a></h6>
-                    <div className="post-meta-single mt-3">
-                        <ul>
-                            <li><i className="fa fa-clock-o"></i>08.22.2020</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div className="col-lg-3 col-sm-6">
-            <div className="single-post-wrap style-white">
-                <div className="thumb">
-                    <img src="assets/img/post/2.png" alt="img"/>
-                        <a className="tag-base tag-orange" href="#">Food</a>
-                </div>
-                <div className="details">
-                    <h6 className="title"><a href="#">Rocket Lab will resume launches no sooner than August 27th</a></h6>
-                    <div className="post-meta-single mt-3">
-                        <ul>
-                            <li><i className="fa fa-clock-o"></i>08.22.2020</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div className="col-lg-3 col-sm-6">
-            <div className="single-post-wrap style-white">
-                <div className="thumb">
-                    <img src="assets/img/post/3.png" alt="img"/>
-                        <a className="tag-base tag-blue" href="#">Tech</a>
-                </div>
-                <div className="details">
-                    <h6 className="title"><a href="#">Google Drive flaw may attackers fool you into install malware</a></h6>
-                    <div className="post-meta-single mt-3">
-                        <ul>
-                            <li><i className="fa fa-clock-o"></i>08.22.2020</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div className="col-lg-3 col-sm-6">
-            <div className="single-post-wrap style-white">
-                <div className="thumb">
-                    <img src="assets/img/post/4.png" alt="img"/>
-                        <a className="tag-base tag-orange" href="#">Food</a>
-                </div>
-                <div className="details">
-                    <h6 className="title"><a href="#">TikTok will sue the US over threatened ban</a></h6>
-                    <div className="post-meta-single mt-3">
-                        <ul>
-                            <li><i className="fa fa-clock-o"></i>08.22.2020</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div> */}
